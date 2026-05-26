f536fde60a7736a07db5f4a7ee92736808873894
Deustavo <gustavo.andrade00@outlook.com>
2026-05-26 15:34:19 -0300

    add vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import './style.css'
 import { Game } from './game/Game'
+import { inject } from "@vercel/analytics"
 
+inject()
 document.fonts.load('bold 16px "Comic Neue"').then(() => {
   new Game()
 })
